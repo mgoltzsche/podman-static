948bf6f8409f5584b8096b4fd33d8b68aa01d6ac
Max Goltzsche <max.goltzsche@gmail.com>
2020-04-16 20:45:10 +0200

    Add port mapping test.

--- make.sh
+++ make.sh
@@ -10,16 +10,28 @@ while [ $# -gt 0 ]; do
 			docker build --force-rm -t ${IMAGE} .
 		;;
 		test)
+			SERVER="echo \$'#!/bin/sh\ntimeout 1 cat - >/dev/null; echo -e \\\"HTTP/1.1 200 OK\n\nup\\\"' > /tmp/healthy && chmod +x /tmp/healthy && timeout 3 nc -l -p 8080 -e /tmp/healthy"
+			PODMAN_PORTMAPPING_TEST='podman run --cgroup-manager=cgroupfs -p 8081:8080 --rm alpine:3.11 /bin/sh -c "'"$SERVER"'" & sleep 3; wget -O - localhost:8081'
 			echo TEST PODMAN AS ROOT '(using CNI)'
 			docker run --rm --privileged --entrypoint /bin/sh \
 				-v "`pwd`/storage-root":/var/lib/containers/storage \
 				${IMAGE} \
-				-c 'podman run --cgroup-manager=cgroupfs --rm alpine:3.10 wget -O /dev/null http://example.org'
+				-c 'podman run --cgroup-manager=cgroupfs --rm alpine:3.11 wget -O /dev/null http://example.org'
+			echo TEST PODMAN AS ROOT - PORT MAPPING
+			docker run --rm --privileged --entrypoint /bin/sh \
+				-v "`pwd`/storage-root":/var/lib/containers/storage \
+				${IMAGE} \
+				-c "$PODMAN_PORTMAPPING_TEST"
 			echo TEST PODMAN AS UNPRIVILEGED USER - NETWORK '(using slirp4netns)'
 			docker run -ti --rm --privileged \
 				-v "`pwd`/storage-user":/podman/.local/share/containers/storage \
 				${IMAGE} \
 				docker run --rm alpine:3.10 wget -O /dev/null http://example.org
+			echo TEST PODMAN AS UNPRIVILEGED USER - PORT MAPPING
+			docker run -ti --rm --privileged \
+				-v "`pwd`/storage-user":/podman/.local/share/containers/storage \
+				${IMAGE} \
+				/bin/sh -c "$PODMAN_PORTMAPPING_TEST"
 			echo TEST PODMAN AS UNPRIVILEGED USER - UID MAPPING '(using fuse-overlayfs)'
 			docker run -ti --rm --privileged \
 				-v "`pwd`/storage-user":/podman/.local/share/containers/storage \
@@ -29,7 +41,7 @@ while [ $# -gt 0 ]; do
 			docker run -ti --rm --privileged -u 100000:100000 --entrypoint /bin/sh \
 				-v "`pwd`/storage-user":/podman/.local/share/containers/storage \
 				${IMAGE} \
-				-c 'set -e; CTR="$(buildah from docker.io/library/alpine:3.10)";
+				-c 'set -e; CTR="$(buildah from docker.io/library/alpine:3.11)";
 					buildah config --cmd "echo hello world" "$CTR";
 					buildah commit "$CTR" buildahtestimage:latest'
 			echo TEST PODMAN BUILD DOCKERFILE AS UNPRIVILEGED USER '(using buildah)'
@@ -45,7 +57,7 @@ while [ $# -gt 0 ]; do
 				${IMAGE} \
 				-c 'set -e;
 					podman build -t podmantestimage -f - . <<-EOF
-						FROM docker.io/library/alpine:3.10
+						FROM docker.io/library/alpine:3.11
 						CMD ["/bin/echo", "hello world"]
 					EOF'
 		;;
