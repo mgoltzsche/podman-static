1a9e3b6c748cb8fa4fce1bbdc49b0b0b6722d935
Max Goltzsche <max.goltzsche@gmail.com>
2021-09-19 00:22:59 +0200

    add support for linux/arm64/v8

--- test/test_helper.bash
+++ test/test_helper.bash
@@ -2,7 +2,7 @@
 
 # ARGS: DOCKER_RUN_OPTS
 testPortMapping() {
-	$DOCKER run --rm -i --privileged --entrypoint /bin/sh $@ - <<-EOF
+	$DOCKER run --rm -i --privileged --entrypoint /bin/sh --pull=never $@ - <<-EOF
 		set -ex
 		podman pull nginxinc/nginx-unprivileged:1.20-alpine
 		podman run -p 8182:8080 --entrypoint=/bin/sh nginxinc/nginx-unprivileged:1.20-alpine -c 'timeout 15 nginx -g "daemon off;"' &
